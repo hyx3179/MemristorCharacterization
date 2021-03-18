d14726e28f3eb84083f44caedb2ffdf2e393be25
hyx3179 <hyx3179@gmail.com>
2021-03-18 22:02:52 +0800

    更新 时序扫描.lua

--- TSB_Script/时序扫描.lua
+++ TSB_Script/时序扫描.lua
@@ -1,63 +1,49 @@
 Voltage = 1
 Voltage_Change = 0.001
 limit_I = 0.001
-timePoint = 110E-6 -- 最小值 66 微秒
+timePoint = 110E-6 -- > 66 微秒
 nplc = 2E-3
-List = {}
 
+
+List = {}
 Points = Voltage / Voltage_Change + 1
 for i = 1, Points do List[i] = 0 + (i - 1) * Voltage_Change end
 for i = 1, Points do List[i + Points] = List[Points - i] end
-
-numberOfSweeps = table.getn(List)
-
--- init
 
 smua.nvbuffer1.clear()
 smua.nvbuffer1.appendmode = 1
 smua.nvbuffer1.collecttimestamps = 1
 smua.nvbuffer1.collectsourcevalues = 1
 
-trigger.timer[1].reset()
-
 smua.reset()
-
--- set smu
 smua.sense = smua.SENSE_REMOTE
-smua.measure.nplc = nplc
-smua.measure.autozero = smua.AUTOZERO_ONCE
 smua.source.func = smua.OUTPUT_DCVOLTS
 smua.source.limiti = limit_I
-smua.source.autorangev = smua.AUTORANGE_OFF -- 必须
+smua.measure.autozero = smua.AUTOZERO_ONCE
+
+smua.measure.nplc = nplc
+smua.source.autorangev = smua.AUTORANGE_OFF
 smua.source.rangev = Voltage
-smua.measure.autorangei = smua.AUTORANGE_OFF -- 必须
+smua.measure.autorangei = smua.AUTORANGE_OFF
 smua.measure.rangei = limit_I
-
-smua.trigger.source.listv(List) -- 必须
+smua.trigger.source.listv(List)
 smua.trigger.measure.i(smua.nvbuffer1)
-
--- Set timer
-trigger.timer[1].stimulus = smua.trigger.ARMED_EVENT_ID -- 当SMU从触发模型的 arm 层移动到触发层时发生
-trigger.timer[1].passthrough = false
-trigger.timer[1].delay = timePoint
-trigger.timer[1].count = numberOfSweeps
---
 smua.trigger.arm.stimulus = trigger.EVENT_ID
-
 smua.trigger.source.stimulus = trigger.timer[1].EVENT_ID
 smua.trigger.source.action = smua.ENABLE
+smua.trigger.measure.action = smua.ENABLE
+smua.trigger.count = table.getn(List)
+trigger.timer[1].reset()
+trigger.timer[1].stimulus = smua.trigger.ARMED_EVENT_ID
+trigger.timer[1].passthrough = false
+trigger.timer[1].delay = timePoint
+trigger.timer[1].count = table.getn(List)
 
--- smua.trigger.measure.stimulus = 0
-smua.trigger.measure.action = smua.ENABLE -- 在扫描期间进行测量
 
--- smua.trigger.arm.count = 1
-smua.trigger.count = numberOfSweeps
 
 smua.source.output = smua.OUTPUT_ON
-
 smua.trigger.initiate()
-
--- StartSweep
+delay(1e-3)
 smua.trigger.arm.set()
 
 function _done()
@@ -72,7 +58,5 @@ function _done()
         delay(0.1)
     until (false)
 end
-
 _done()
-
 reset()
